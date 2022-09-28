78047f79c221b6e9cb5c4f77a5ad197365f42508
Kain Nhantumbo <nhantumbok@gmail.com>
2022-09-28 10:14:22 +0200

    docs: added license file.

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,9 +1,19 @@
 import db from '../database/connection';
 import { Application as IApp } from 'express';
 
-async function bootstrap(app: IApp, port: string | number, db_uri: string) {
+type Port = string | number;
+
+/**
+ * @param app express.Application
+ * @param port string | number
+ * @param db_uri database connection string
+ * @returns Promise<void>
+ * @instance starts the server
+ */
+async function bootstrap(app: IApp, port: Port, db_uri: string) {
 	try {
 		await db(db_uri);
+		console.log('Connection to database has been established successfuly.');
 		app.listen(port, () => console.log('Server listening on port ', port));
 	} catch (err) {
 		console.error(err);
